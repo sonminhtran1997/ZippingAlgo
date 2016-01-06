6b62b6ac41877d81fdd46cf088511b27117ff6dc
Royden Luckey <rluckey@uw.edu>
2016-01-06 11:41:14 -0800

    Update Javadoc from addWork() to add() for FIFOWorkList and LIFOWorkList

--- src/cse332/interfaces/worklists/FIFOWorkList.java
+++ src/cse332/interfaces/worklists/FIFOWorkList.java
@@ -3,7 +3,7 @@ package cse332.interfaces.worklists;
 /**
  * A subclass of WorkList that stores its elements in FIFO order.
  * All subclasses of this class implicitly agree to the contract to
- * be a FIFO queue.  That is, addWork() must add to the "end" and
+ * be a FIFO queue.  That is, add() must add to the "end" and
  * next() must remove from the "beginning".
  * 
  * @author Adam Blank
--- src/cse332/interfaces/worklists/LIFOWorkList.java
+++ src/cse332/interfaces/worklists/LIFOWorkList.java
@@ -3,7 +3,7 @@ package cse332.interfaces.worklists;
 /**
  * A subclass of WorkList that stores its elements in LIFO order.
  * All subclasses of this class implicitly agree to the contract to
- * be a LIFO queue.  That is, addWork() must add to the "top" and
+ * be a LIFO queue.  That is, add() must add to the "top" and
  * next() must remove from the "top".
  * 
  * @author Adam Blank
