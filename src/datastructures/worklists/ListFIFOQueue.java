bcba34e142be0c24c67e69b1d0ec8bad9f6f66b8
maxtian3 <maxtian3@vdilab14.csenetid.cs.washington.edu>
2018-01-05 14:32:44 -0800

    finish node class and adding method

--- src/datastructures/worklists/ListFIFOQueue.java
+++ src/datastructures/worklists/ListFIFOQueue.java
@@ -8,19 +8,42 @@ import cse332.interfaces.worklists.FIFOWorkList;
  * for method specifications.
  */
 public class ListFIFOQueue<E> extends FIFOWorkList<E> {
-    
+    private class Node<E>{
+    	public E data;
+    	public Node next;
+    	public Node(E data) {
+    		this.data = data;
+    		this.next = null;
+    	}
+    	public E getData() {
+    		return this.data;
+    	}
+    	public void setNext(Node next) {
+    		this.next = next;
+    	}
+    }
+    public Node head, tail;
     public ListFIFOQueue() {
-        throw new NotYetImplementedException();
+        this.head = null;
+        this.tail = null;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+    	Node newNode = new Node(work);
+        if (head == null) {
+			this.head = newNode;
+			this.tail = newNode;
+		}
+        else {
+        	tail.setNext(newNode);
+        	tail = newNode;
+        }
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+    	
     }
 
     @Override
