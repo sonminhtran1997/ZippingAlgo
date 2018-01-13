41e4845016d95ab1e8b18514ef87b58231a46e66
smt21 <smt21@percyharvin.csenetid.cs.washington.edu>
2018-01-13 15:53:33 -0800

    Finish MinFourHeap

--- src/datastructures/worklists/MinFourHeap.java
+++ src/datastructures/worklists/MinFourHeap.java
@@ -1,6 +1,9 @@
 package datastructures.worklists;
 
 import cse332.interfaces.worklists.PriorityWorkList;
+
+import java.util.NoSuchElementException;
+
 import cse332.exceptions.NotYetImplementedException;
 
 /**
@@ -10,38 +13,117 @@ import cse332.exceptions.NotYetImplementedException;
 public class MinFourHeap<E extends Comparable<E>> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
-    
+    private int capacity;
+    private int occupied;
     public MinFourHeap() {
-        throw new NotYetImplementedException();
+    	this.occupied = 0;
+    	this.capacity = 10;
+        data = (E[]) new Comparable[this.capacity];
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+        return this.occupied > 0;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+    	if (this.occupied == this.capacity) {
+    		this.capacity *= 2;
+			E[] temp = (E[]) new Comparable[this.capacity];
+			for (int i = 0; i < this.occupied; i++) {
+				temp[i] = data[i];
+			}
+			data = temp;
+		}
+    	this.occupied++;
+    	int i = percolateUp(occupied - 1, work);
+		data[i] = work;
+		
     }
-
+    private int percolateUp(int hole, E work) {
+    	while (hole > 0 && work.compareTo(data[(hole-1)/4]) < 0) {
+			data[hole] = data[(hole-1)/4];
+			hole = (hole-1) / 4;
+		}
+    	return hole;
+    }
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+    	if (!hasWork()) {
+			throw new NoSuchElementException();
+		}
+        return data[0];
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if (!hasWork()) {
+			throw new NoSuchElementException();
+		}
+        E ans = data[0];
+        int hole = percolateDown(0, data[occupied-1]);
+        data[hole] = data[occupied-1];
+        occupied--;
+        return ans;
+    }
+    private int percolateDown(int hole, E work) {
+    	int first, second, third, fourth;
+    	int target = 0;
+    	E min = data[0];
+    	while (4*hole + 2 <= occupied) {
+			first = 4*hole + 1;
+			second = 4*hole + 2;
+			third = 4*hole + 3;
+			fourth = 4*hole + 4;
+			if (fourth <= occupied - 1) {
+				target = findMinIndex(data, first, fourth);
+			}
+			else if(third <= occupied - 1) {
+				target = findMinIndex(data, first, third);
+			}
+			else if(second <= occupied - 1) {
+				target = findMinIndex(data, first, second);
+			}
+			else{
+				target = first;
+			}
+			if (data[target].compareTo(work) < 0) {
+				data[hole] = data[target];
+				hole = target;
+			}
+			else {
+				break;
+			}
+		}
+    	return hole;
+    }
+    public int findMinIndex(E[] sample, int starting, int ending) {
+    	int min = starting;
+    	for (int i = starting + 1; i <= ending; i++) {
+			if (sample[i].compareTo(sample[min]) < 0) {
+				min = i;
+			}
+		}
+    	return min;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.occupied;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+    	this.occupied = 0;
+    	this.capacity = 10;
+        data = (E[])new Comparable[capacity];
+    }
+    public String toString() {
+    	String result = "";
+    	for (int i = 0; i < data.length; i++) {
+			result += data[i] + " ";
+		}
+    	return result;
     }
 }
