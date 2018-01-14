f840a4c904bb2b598db65bb5aab0461fc309c5d3
maxtian3 <maxtian3@beastmode.csenetid.cs.washington.edu>
2018-01-13 21:32:40 -0800

    finish

--- src/datastructures/dictionaries/HashTrieMap.java
+++ src/datastructures/dictionaries/HashTrieMap.java
@@ -38,26 +38,79 @@ public class HashTrieMap<A extends Comparable<A>, K extends BString<A>, V> exten
 
     @Override
     public V insert(K key, V value) {
-        throw new NotYetImplementedException();
+		if (key == null || value == null) {
+			throw new IllegalArgumentException();
+		}
+		Iterator it = key.iterator();
+		HashTrieNode current = (HashTrieNode) this.root;
+		HashTrieNode addVal = new HashTrieNode(value);
+		while (it.hasNext()) {
+			A singleK = (A) it.next();
+			if (current.pointers.get(singleK) != null) {
+				current = current.pointers.get(singleK);
+			} else {
+				current.pointers.put(singleK, new HashTrieNode());
+				current = current.pointers.get(singleK);
+			}
+		}
+		V old = current.value;
+		current.value = value;
+		return old;
     }
 
     @Override
     public V find(K key) {
-        throw new NotYetImplementedException();
+    	if (key == null) {
+			throw new IllegalArgumentException();
+		}
+        Iterator it = key.iterator();
+        HashTrieNode current = (HashTrieNode)this.root;
+        while(it.hasNext()) {
+        	A singleK = (A) it.next();
+        	current = current.pointers.get(singleK);
+			if(current ==null) {
+				return null;
+			}
+        }
+        return 	current.value;
     }
 
     @Override
     public boolean findPrefix(K key) {
-        throw new NotYetImplementedException();
+        if(key == null) {
+        	throw new IllegalArgumentException();
+        }
+        Iterator it = key.iterator();
+        HashTrieNode current = (HashTrieNode)this.root;
+        while(it.hasNext()) {
+        	A singleK = (A) it.next();
+        		
+        	current = current.pointers.get(singleK);
+			if(current ==null) {
+				return false;
+			}
+        }
+        if (current.pointers.isEmpty()) {
+			if (current.value != null) {
+				return true;
+			}
+			return false;
+		}
+        return true;
     }
 
     @Override
     public void delete(K key) {
-        throw new NotYetImplementedException();
+//    	HashTrieNode current = (HashTrieNode)this.root;
+//    	Iterator it = key.iterator();
+//        while(it.hasNext()) {
+//        	A singleK = (A) it.next();
+//        	current = current.pointers.get(singleK);
+//        }
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        this.root = new HashTrieNode();
     }
 }
